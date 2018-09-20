19e6a57c5c4738df46ed9f66b0562ad66cd4bd04
Johan PONIN <johan.ponin.pro@gmail.com>
2018-09-20 12:37:01 +0200

    DCR

--- src/main.rs
+++ src/main.rs
@@ -11,26 +11,12 @@ struct Thing { s:String }
 
 fn urls(conn:Connection) -> Result<Vec<String>, rusqlite::Error> {
     let mut stmt = try!(conn.prepare("SELECT * FROM urls LIMIT 1000"));
-    // let mut rows = try!(stmt.query(&[]));
-    // let mut urls = Vec::new();
-    // while let Some(result_row) = rows.next() {
-    //     let row = try!(result_row);
-    //     urls.push(row.get(0));
-    // }
-    // Ok(urls)
     let it = stmt.query_map(&[], |row| Thing { s: row.get("url") }).unwrap();
     let mut urls = Vec::new();
     for t in it {
         urls.push(t?.s)
     }
     Ok(urls)
-}
-
-fn _reqwest_test() {
-    match reqwest::get("https://api.ipify.org?format=json") {
-        Ok(r) => { println!("{:?}", r) }
-        _ => { panic!("ip fail") }
-    }
 }
 
 fn _dump(s:Map<String,String>) {
@@ -55,18 +41,19 @@ fn main() {
                             println!("{} HEAD {}", i, u);
                             client.head(u.as_str()).send() })
                         .collect::<Vec<Result<Response, reqwest::Error>>>();
+
                     println!("heads {}", rs.len());
                     let (a,_) = rs.as_slice().split_at(lim);
                     println!("heads {:?}", a);
-                    // dump(_rs);
+
                     println!("found: {:?} url(s)", urls.len());
                     let (a,_) = urls.as_slice().split_at(lim);
                     println!("first {}: {:?}", lim, a);
                 }
-                _ => panic!("fn urls failed?")
+                _ => panic!("sqlite query `urls` failed")
             }
         }
-        _ => panic!("connection to file `{}` failed?", path)
+        _ => panic!("connection to file `{}` failed", path)
     }
     println!("bye.");
 }
