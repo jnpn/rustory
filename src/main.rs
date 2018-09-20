c29ba0a19af00df7d3350e2343ca1877a69f496b
Johan PONIN <johan.ponin.pro@gmail.com>
2018-09-20 14:04:50 +0200

    debug trait for Thing, more fields from sqlite

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,32 @@ extern crate rusqlite;
 
 use rusqlite::{Connection, OpenFlags};
 use reqwest::{Client, Response};
+use std::fmt;
 use std::iter::Map;
 
-struct Thing { s:String }
+struct Thing {
+    // id|url|title|visit_count|typed_count|last_visit_time|hidden
+    i:i64,
+    s:String,
+}
 
-fn urls(conn:Connection) -> Result<Vec<String>, rusqlite::Error> {
-    let mut stmt = try!(conn.prepare("SELECT * FROM urls LIMIT 1000"));
-    let it = stmt.query_map(&[], |row| Thing { s: row.get("url") }).unwrap();
+impl std::fmt::Debug for Thing {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> fmt::Result {
+        write!(f,"({} {})", self.i, self.s)
+    }
+}
+
+fn urls(conn:Connection) -> Result<Vec<Thing>, rusqlite::Error> {
+    let mut stmt = try!(conn.prepare("SELECT * FROM urls"));
+    let it = stmt
+        .query_map(&[], |row| Thing {
+            i: row.get("id"),
+            s: row.get("url"),
+        })
+        .unwrap();
     let mut urls = Vec::new();
     for t in it {
-        urls.push(t?.s)
+        urls.push(t?)
     }
     Ok(urls)
 }
@@ -26,7 +42,6 @@ fn _dump(s:Map<String,String>) {
 }
 
 fn main() {
-    let lim = 32;
     let path = String::from("History");
     let client = Client::new();
     let c = Connection::open_with_flags(&path, OpenFlags::SQLITE_OPEN_READ_ONLY);
@@ -34,12 +49,13 @@ fn main() {
         Ok(conn) => {
             match urls(conn) {
                 Ok(urls) => {
+                    let lim = 32;
                     let rs = urls
                         .iter()
                         .enumerate()
-                        .map(|(i,u)| {
-                            println!("{} HEAD {}", i, u);
-                            client.head(u.as_str()).send() })
+                        .map(|(i,t)| {
+                            println!("{} HEAD {:?}", i, t);
+                            client.head(t.s.as_str()).send() })
                         .collect::<Vec<Result<Response, reqwest::Error>>>();
 
                     println!("heads {}", rs.len());
@@ -49,6 +65,7 @@ fn main() {
                     println!("found: {:?} url(s)", urls.len());
                     let (a,_) = urls.as_slice().split_at(lim);
                     println!("first {}: {:?}", lim, a);
+
                 }
                 _ => panic!("sqlite query `urls` failed")
             }
