e63591bfcbf50d05bed5d599777e66cbfd472079
Johan PONIN <johan.ponin.pro@gmail.com>
2018-09-28 08:37:29 +0200

    first working prototype

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use serde::ser::{SerializeStruct, Serializer};
 use rusqlite::{Connection, OpenFlags};
 use reqwest::{Client, Response};
 use std::collections::HashMap;
+use std::fmt::{Debug, Formatter};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Thing {
@@ -17,8 +18,8 @@ struct Thing {
     s:String,
 }
 
-fn urls(conn:Connection) -> Result<Vec<Thing>, rusqlite::Error> {
-    let mut stmt = try!(conn.prepare("SELECT * FROM urls LIMIT 32"));
+fn urls(conn:Connection) -> std::result::Result<Vec<Thing>, rusqlite::Error> {
+    let mut stmt = try!(conn.prepare("SELECT * FROM urls"));
     let it = stmt
         .query_map(&[], |row| Thing {
             i: row.get("id"),
@@ -32,29 +33,45 @@ fn urls(conn:Connection) -> Result<Vec<Thing>, rusqlite::Error> {
 
 struct ResponseWrapper { r: Response }
 
+impl Debug for ResponseWrapper {
+    fn fmt(&self, formatter: &mut Formatter) -> std::fmt::Result {
+        self.r.fmt(formatter)
+    }
+}
+
 impl serde::Serialize for ResponseWrapper {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let m:HashMap<String,String> = self.r.headers()
+        let m:HashMap<&str,&str> = self.r.headers()
             .iter()
-            .map(|(k,v)| (String::from(k.as_str()),String::from(v.to_str().unwrap())))
-            .collect::<HashMap<String,String>>();
+            .map(|(k,v)| {
+                match (k.as_str(),v.to_str()) {
+                    (k,Ok(v)) => (k,v),
+                    (k,Err(_)) => (k,"error @ v.to_str()")
+                }
+            })
+            .collect::<>();
 
-        let mut s = serializer.serialize_struct("Resp", 2)?;
+        let mut s = serializer.serialize_struct("Resp", 4)?;
+        s.serialize_field("url", &self.r.url().as_str())?;
+        s.serialize_field("version", &format!("{:?}",&self.r.version()))?;
         s.serialize_field("status_code", &self.r.status().as_u16())?;
         s.serialize_field("headers", &m)?;
         s.end()
     }
 }
 
-fn main() {
+fn all () {
     let path = String::from("History");
     let client = Client::new();
+
     if let Ok(c) = Connection::open_with_flags(&path, OpenFlags::SQLITE_OPEN_READ_ONLY) {
+        info!("{} -> sqlite3: ok", path);
         if let Ok(all) = urls(c) {
             all.iter()
+                .inspect(|t| eprintln!("[info] {}", t.s.as_str()))
                 .map(|t| { client.head(t.s.as_str()).send() })
                 .for_each(|m| {
                     if let Ok(r) = m {
@@ -65,4 +82,10 @@ fn main() {
                 })
         }
     }
+}
+
+fn main() {
+
+    all()
+
 }
