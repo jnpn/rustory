d3ea87819708d35eeed3cfd0a0cf45d2da6f6ec9
Johan PONIN <johan.ponin.pro@gmail.com>
2018-10-01 23:45:48 +0200

    added ireduce

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,8 @@
 fn with<T>(mv:T, mf:fn(&T)->()) -> T { mf(&mv); mv }
 fn withed<T>(mv:T, mf:fn(T)->T) -> T { mf(mv) }
 fn co<U,V>(a:U, f:fn(U)->V) -> V { f(a) }
+
+fn ireduce<T>(a:T, i:Iterable, f:fn(T)->T) -> T {
+    for e in i { f(a,e) };
+    a
+}
