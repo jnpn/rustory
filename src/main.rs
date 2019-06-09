1710f034c72760b64ad5970cc58cd595785aa9dc
Johan PONIN <johan.ponin.pro@gmail.com>
2019-06-09 20:46:28 +0200

    forgot..

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
 extern crate reqwest;
+extern crate rusqlite;
 extern crate serde;
 extern crate serde_json;
-extern crate rusqlite;
 
-use serde::ser::{SerializeStruct, Serializer};
-use rusqlite::{Connection, OpenFlags};
 use reqwest::{Client, Response};
+use rusqlite::{Connection, OpenFlags};
+use serde::ser::{SerializeStruct, Serializer};
 use std::collections::HashMap;
 use std::fmt::{Debug, Formatter, Result as Res};
+
+/***
+ *
+ * description: sqlite | rust | json
+ *
+ * 1) sqlite db (id,url,title,...)* -> Thing(i,s)*
+ * 2) Thing(i,s)* -> HTTP HEAD(s)* -> ResponsWrapper( )* -> IO JSON
+ *
+ ***/
 
 #[derive(Debug)]
 struct Thing {
     // id|url|title|visit_count|typed_count|last_visit_time|hidden
-    i:i64,
-    s:String,
+    i: i64,
+    s: String,
 }
 
-fn urls(conn:Connection) -> Result<Vec<Thing>, rusqlite::Error> {
+fn urls(conn: Connection) -> Result<Vec<Thing>, rusqlite::Error> {
     let mut stmt = try!(conn.prepare("SELECT * FROM urls"));
     let it = stmt
         .query_map(&[], |row| Thing {
@@ -29,7 +38,9 @@ fn urls(conn:Connection) -> Result<Vec<Thing>, rusqlite::Error> {
     Ok(it)
 }
 
-struct ResponseWrapper { r: Response }
+struct ResponseWrapper {
+    r: Response,
+}
 
 impl Debug for ResponseWrapper {
     fn fmt(&self, formatter: &mut Formatter) -> Res {
@@ -42,31 +53,32 @@ impl serde::Serialize for ResponseWrapper {
     where
         S: Serializer,
     {
-        let m:HashMap<&str,&str> = self.r.headers()
+        let m: HashMap<&str, &str> = self
+            .r
+            .headers()
             .iter()
-            .map(|(k,v)| {
-                match (k.as_str(),v.to_str()) {
-                    (k,Ok(v)) => (k,v),
-                    (k,Err(_)) => (k,"error @ v.to_str()")
-                }
+            .map(|(k, v)| match (k.as_str(), v.to_str()) {
+                (k, Ok(v)) => (k, v),
+                (k, Err(_)) => (k, "error @ v.to_str()"),
             })
-            .collect::<>();
+            .collect();
 
         let mut s = serializer.serialize_struct("Resp", 4)?;
         s.serialize_field("url", &self.r.url().as_str())?;
-        s.serialize_field("version", &format!("{:?}",&self.r.version()))?;
+        s.serialize_field("version", &format!("{:?}", &self.r.version()))?;
         s.serialize_field("status_code", &self.r.status().as_u16())?;
         s.serialize_field("headers", &m)?;
         s.end()
     }
 }
 
-fn all () {
+fn all() {
     let path = String::from("History");
     let client = Client::new();
 
-    let jsonify = |s:&String| {
+    let jsonify = |s: &String| {
         let v = client.head(s.as_str()).send();
+        // not cute
         if let Ok(r) = v {
             if let Ok(j) = serde_json::to_string(&ResponseWrapper { r }) {
                 Some(j)
@@ -82,8 +94,12 @@ fn all () {
         if let Ok(all) = urls(c) {
             all.iter()
                 .inspect(|t| eprintln!("[info] {}", t.s.as_str()))
-                .map(|t| { jsonify(&t.s) })
-                .for_each(|s| { if let Some(s) = s { println!("{}", s) } })
+                .map(|t| jsonify(&t.s))
+                .for_each(|s| {
+                    if let Some(s) = s {
+                        println!("{}", s)
+                    }
+                })
         }
     }
 }
